94ea5910a110f082804ba2b5051e91665ba05262
Divya <divyak1898@gmail.com>
2019-01-06 21:50:26 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -5,7 +5,7 @@
 
 void main() {
 	int a[1000],b[1000],c[1000],sum,i;
-	for(i=0;i<100;i++)
+	for(i=0;i<1000;i++)
 	    a[i] = b[i] = i;
     	#pragma omp parallel for reduction(+:sum)
     	for(i=0;i<1000;i++) 
